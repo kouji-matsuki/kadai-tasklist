af00b88da448be8cb6695f57475cf681dbce7300
kouji-matsuki <boranchimk1216@gmail.com>
2022-08-12 12:01:48 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_10_123257) do
+ActiveRecord::Schema.define(version: 2022_08_12_111129) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
